10ebf73f00ab812832040f5a88f1b813b7ebdcb8
Roy Golan <royg21@gmail.com>
2024-03-23 19:57:23 +0200

    Initial commit

---
+++ lua/venkat/init.lua
@@ -0,0 +1,76 @@
+M = {}
+
+M.config = {}
+M.config.pattern = "main.go"
+M.config.languages = {
+    go = { cmdline = "go run %s", pattern = "main.go" },
+    java = { cmdline = "java %s", pattern = "Main.java" },
+    rust = { cmdline = "cargo run %s", pattern = "main.rs" },
+    python = { cmdline = "python %s", pattern = "main.py" }
+}
+
+M.config.getPattern = function()
+    local pattern = ""
+    for _, value in pairs(M.config.languages) do
+        pattern = pattern .. "," .. value.pattern
+    end
+    return pattern
+end
+
+local withFilename = function(lang, filename)
+    local cmd = {}
+    local str = string.format(M.config.languages[lang].cmdline, string.match(filename, "^.+/(.+)$"))
+    for i in string.gmatch(str, "%S+") do
+        table.insert(cmd, i)
+    end
+    return cmd
+end
+
+M.execute = function()
+    local filetype = vim.bo.filetype
+    if M.config.languages[filetype] == nil then
+        print("no command for file type " .. filetype)
+        return
+    end
+    local filename = vim.api.nvim_buf_get_name(0)
+    local newcmd = withFilename(filetype, filename)
+
+    local popup = function(bufnr, data)
+        print("data is " .. vim.inspect(data))
+        if data then
+            local floatbuf = vim.api.nvim_create_buf(false, true)
+            vim.api.nvim_buf_set_keymap(floatbuf, 'n', '<Esc>', ':close<CR>', {
+                silent = true,
+                nowait = true,
+                noremap = true
+            })
+            vim.api.nvim_buf_set_lines(floatbuf, 0, 0, false, data)
+            vim.api.nvim_open_win(floatbuf, bufnr, {
+                relative = "cursor",
+                anchor = "NE",
+                height = 10,
+                width = 80,
+                col = 60,
+                row = 1,
+                border = "rounded",
+                style = "minimal"
+            })
+        end
+    end
+
+    local bufnr = vim.api.nvim_get_current_buf()
+    vim.fn.jobstart(newcmd, {
+        stdout_buffered = true,
+        stderr_buffered = true,
+        on_stdout = function(_, data)
+            popup(bufnr, data)
+        end,
+        on_stderr = function(_, data)
+            popup(bufnr, data)
+        end,
+        on_exit = function(_, data)
+        end
+    })
+end
+
+return M
---
+++ plugin/venkat/init.lua
@@ -0,0 +1,10 @@
+vim.api.nvim_create_autocmd("BufWritePost", {
+    group = vim.api.nvim_create_augroup("venkatmode", { clear = true }),
+    pattern = require("venkat").config.getPattern(),
+    callback = function()
+        require("venkat").execute()
+    end,
+})
+
+
+
