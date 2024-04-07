dd49a2a6b00fd324cdb5ebb0fbdc072b3acd9639
Roy Golan <royg21@gmail.com>
2024-04-07 21:49:59 +0300

    Unify stdout and stderr buffers, and print as we go
    Signed-off-by: Roy Golan <royg21@gmail.com>

--- lua/venkat/init.lua
+++ lua/venkat/init.lua
@@ -1,12 +1,12 @@
 M = {}
 
 M.config = {}
-M.config.pattern = "main.go"
 M.config.languages = {
     go = { cmdline = "go run %s", pattern = "main.go" },
     java = { cmdline = "java %s", pattern = "Main.java" },
     rust = { cmdline = "cargo run %s", pattern = "main.rs" },
     python = { cmdline = "python %s", pattern = "main.py" },
+    c = { cmdline = "zig run -lc %s", pattern = "main.c" },
     zig = { cmdline = "zig run %s", pattern = "main.zig" },
 }
 
@@ -24,51 +24,58 @@ local withFilename = function(lang, filename)
     for i in string.gmatch(str, "%S+") do
         table.insert(cmd, i)
     end
+
+    cmd = {}
+    str = string.format(M.config.languages[lang].cmdline, filename)
+    for i in string.gmatch(str, "%S+") do
+        table.insert(cmd, i)
+    end
+
+    --print("cmd is " .. vim.inspect(cmd))
     return cmd
 end
 
 M.execute = function()
+    --print("executing ... ")
     local filetype = vim.bo.filetype
     if M.config.languages[filetype] == nil then
         return
     end
     local filename = vim.api.nvim_buf_get_name(0)
     local newcmd = withFilename(filetype, filename)
+    local floatBufId = vim.api.nvim_create_buf(false, true)
+    local windowId = vim.api.nvim_open_win(floatBufId, true, {
+        relative = "cursor",
+        border = "rounded",
+        style = "minimal",
+        anchor = "NE",
+        height = 10,
+        width = 80,
+        col = 60,
+        row = 1,
+    })
 
-    local popup = function(bufnr, data)
-        if data then
-            local floatbuf = vim.api.nvim_create_buf(false, true)
-            vim.api.nvim_buf_set_keymap(floatbuf, 'n', '<Esc>', ':close<CR>', {
+    local popup = function(bufnum, data)
+            vim.api.nvim_buf_set_keymap(bufnum, 'n', '<Esc>', ':close<CR>', {
                 silent = true,
                 nowait = true,
                 noremap = true
+
             })
-            vim.api.nvim_buf_set_lines(floatbuf, 0, 0, false, data)
-            vim.api.nvim_open_win(floatbuf, bufnr, {
-                relative = "cursor",
-                border = "rounded",
-                style = "minimal",
-                anchor = "NE",
-                height = 10,
-                width = 80,
-                col = 60,
-                row = 1,
-            })
-        end
+            vim.api.nvim_buf_set_lines(bufnum, -1, -1, false, data)
+            vim.api.nvim_set_current_win(windowId)
     end
 
-    local bufnr = vim.api.nvim_get_current_buf()
+
     vim.fn.jobstart(newcmd, {
-        stdout_buffered = true,
-        stderr_buffered = true,
+        stdout_buffered = false,
+        stderr_buffered = false,
         on_stdout = function(_, data)
-            popup(bufnr, data)
+            popup(floatBufId, data)
         end,
         on_stderr = function(_, data)
-            popup(bufnr, data)
+            popup(floatBufId, data)
         end,
-        on_exit = function(_, data)
-        end
     })
 end
 
